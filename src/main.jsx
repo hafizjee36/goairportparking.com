68e4b276d463d15bfa709734d304df44fdf3e72e
MUHAMMAD WASEEM AKHTAR <your-email@example.com>
2026-03-11 16:18:30 +0500

    optimization changes only

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
 import theme from "./theme/index.js";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/700.css";
-import "./index.css";
 
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
+
+import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
